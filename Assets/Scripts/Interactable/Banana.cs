8be137ad4b3c40f1f4771f38cbe46f4f4cd16dff
Mikyle Mosquera <mikylemosquera@gmail.com>
2024-06-19 01:38:36 -0700

    Updated banana audio

--- Assets/Scripts/Interactable/Banana.cs
+++ Assets/Scripts/Interactable/Banana.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Banana : InteractableObject
 {
+    [SerializeField] private float _sfxVolume = 0.25f;
+    
     public override void Interact(HoboInteractionController hoboInteractionController)
     {
+        AudioManager.instance.PlayGlobalAudio("[08] Eating Banana", _sfxVolume);
+        
         hoboInteractionController.GetComponent<HoboCharacterController>().SetAbleToJump(true);
         Destroy(gameObject);
     }
