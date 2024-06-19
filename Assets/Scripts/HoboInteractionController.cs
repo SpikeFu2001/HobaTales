4f3ff0fc37838b7b32c22691c7e8b1faf2fa216f
Mikyle Mosquera <mikylemosquera@gmail.com>
2024-06-18 23:20:30 -0700

    Fixed

--- Assets/Scripts/HoboInteractionController.cs
+++ Assets/Scripts/HoboInteractionController.cs
@@ -100,7 +100,7 @@ public class HoboInteractionController : MonoBehaviour
 
         if (closestObject != null)
         {
-            closestObject.Interact();
+            closestObject.Interact(this);
         }
     }
 
